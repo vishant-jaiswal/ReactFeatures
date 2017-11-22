f807c4bd3c0e7cac8d39c23af818960e5df2a6a2
Vishant <vishantjaiswal2k@gmail.com>
2017-11-22 15:35:54 +0530

    A constructor is added to Component1 and a state variable is added and used in render function

--- src/Components/Component1.js
+++ src/Components/Component1.js
@@ -4,9 +4,17 @@ import {View,Text } from 'react-native';
 
 class Component1 extends Component{
 
+    constructor(){
+        super();
+        this.state = {
+            name:'Component1',
+        }
+    }
+
     render(){
         return(
             <View>
+                <Text> {this.state.name}</Text>
                 <Text> This is Comming from Component1.js</Text>
                 <Text>{this.props.message}</Text>
             </View>
